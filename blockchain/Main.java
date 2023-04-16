29c51f696cf34cb2e46f168379e1b5b8ec57ed71
Tatyana Kharitonova <my@mail.com>
2023-04-16 13:23:28 +0300

    feat: stage 2/6 A proof of work concept

--- blockchain/Main.java
+++ blockchain/Main.java
@@ -1,16 +1,28 @@
 package blockchain;
 
+import java.util.Scanner;
+
+import blockchain.logic.Block;
+import blockchain.logic.BlockManager;
+
 public class Main {
+
+    private static final String FILE = "blockchain.obj";
+
     public static void main(String[] args) {
 
-        Block[] blockchain = new Block[5];
-        blockchain[0] = new Block("0");
-        for (int i = 1; i < blockchain.length; i++) {
-            blockchain[i] = new Block(blockchain[i - 1].getHash());
-        }
+        Block block = null;
 
-        for (Block block : blockchain) {
-            System.out.println(block + "\n");
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter how many zeros the hash must start with: ");
+
+        BlockManager manager = new BlockManager(scanner.nextInt(), block);
+
+        Block b1 = manager.createBlock();
+        Block b2 = manager.createBlock();
+        Block b3 = manager.createBlock();
+        Block b4 = manager.createBlock();
+        Block b5 = manager.createBlock();
+
     }
 }
